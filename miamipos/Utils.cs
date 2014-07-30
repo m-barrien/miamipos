7fe263433a3dbdf1d3d0f48b372e91cf294972e5
Marcelo Barrientos <mb33135@gmail.com>
2014-07-30 16:08:09 -0400

    Se creo la clase Producto para modularizar el manejo del editor de productos

--- miamipos/Utils.cs
+++ miamipos/Utils.cs
@@ -21,4 +21,50 @@ namespace miamiPOS
     {
         public static float Gramos { get; set; }
     }
+    public class Producto
+    {
+        int plu;
+        string barcode;
+        string name;
+        int price;
+        int id_categoria;
+        bool pesable;
+        public Producto(int plu, string barcode, string name, int price, int id_categoria, bool pesable)
+        {
+            this.plu = plu;
+            this.barcode = barcode;
+            this.name = name;
+            this.price = price;
+            this.id_categoria = id_categoria;
+            this.pesable = pesable;
+        }
+        public string toSQL(bool editar)
+        {
+            string pesableSQL="FALSE";
+            if(pesable)
+            {
+                pesableSQL="TRUE";
+            }
+            if (barcode.Length <= 5)
+            {
+                this.barcode = "NULL"; //digitos menores que cinco cuentan como un PLU en el programa
+            }
+            else
+            {
+                this.barcode = "'" + this.barcode + "'";
+            }
+            if (!editar)
+            {
+                string output = String.Format("INSERT INTO producto (plu, barcode, nombre, precio, id_categoria, pesable) VALUES ({0},{1},'{2}',{3},{4},{5})",
+                              this.plu, this.barcode,this.name,this.price,this.id_categoria,pesableSQL);
+                return output;
+            }
+            else
+            {
+                string output = String.Format("UPDATE producto SET barcode={1}, nombre='{2}', precio={3}, id_categoria={4}, pesable={5}, last_change=now() WHERE plu={0}",
+                              this.plu, this.barcode,this.name,this.price,this.id_categoria,pesableSQL);
+                return output;
+            }
+        }
+    }
 }
